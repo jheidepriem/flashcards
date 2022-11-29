2892b6b34f62bfe017cde1bd776d79f7284de6d9
Josephine Heidepriem <jheidepriem@gmail.com>
2022-11-29 15:47:58 -0600

    add it blocks to deck-test

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -3,19 +3,27 @@ const expect = chai.expect;
 const Deck = require('../src/Deck.js');
 const Card = require('../src/Card.js');
 
-describe('Deck', function(){
-
+describe('Deck', () => {
   let deck;
   let card1;
   let card2;
   let card3;
-  beforeEach(function() {
+  beforeEach(() => {
     card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
     card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
     deck = new Deck([card1, card2, card3]);
   })
 
+  it('should be a function', () => {
+    expect(Deck).to.be.a('function');
+  })
 
-  
+  it('should be an instance of Deck', () => {
+    expect(deck).to.be.an.instanceof(Deck);
+  })
+
+  it('should be able to count cards in the deck', () => {
+    expect(deck.countCards()).to.equal(3);
+  })
 })
