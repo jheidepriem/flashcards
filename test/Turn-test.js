8a0aceb6bfd7ae0f99ab1c88909cde86540a1ef5
Josephine Heidepriem <jheidepriem@gmail.com>
2022-11-28 18:40:43 -0600

    add beforeEach to turn-test

---
+++ test/Turn-test.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Test = require('../src/Test.js');
+const Card = require('../src/Card.js');
+
+describe('Turn', function() {
+
+  let card;
+  let turn;
+  this.beforeEach(function() {
+    card = new Card();
+    turn = new turn();
+  });
+
+
+})
