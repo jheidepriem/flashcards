fb578dce76072ef49a3f539e3adfba7ddd24524c
Josephine Heidepriem <jheidepriem@gmail.com>
2022-11-28 19:19:15 -0600

    add several it blocks required for the turn-test

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Test = require('../src/Test.js');
+const Turn = require('../src/Turn.js');
 const Card = require('../src/Card.js');
 
 describe('Turn', function() {
@@ -9,9 +9,33 @@ describe('Turn', function() {
   let card;
   let turn;
   this.beforeEach(function() {
-    card = new Card();
-    turn = new turn();
+    card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    turn = new Turn('pug', card);
   });
 
+  it.skip('should be a function', function(){
+    expect(turn).to.be.an.instanceof(Turn);
+  })
 
+  it.skip('should be an instance of Card', function() {
+    expect(turn).to.be.an.instanceof(Turn);
+  })
+
+  it.skip('should have a player guess', function() {
+    expect(turn.guess).to.equal('pug');
+  })
+
+  it.skip('should have player\'s current card in play', function() {
+    expect(this.card).to.be.an.instanceof(Card);
+  })
+
+  it.skip('should return player\'s guess', function() {
+    turn.returnGuess()
+    expect(turn.returnGuess).to.deep.equal('pug');
+  })
+
+  it.skip('should return player\'s card', function() {
+    turn.returnCard()
+    expect(turn.returnCard).to.be.an.instanceof(Card)
+  })
 })
