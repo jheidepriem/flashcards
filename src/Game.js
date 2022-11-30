45d7dc6c82237b0fb4caf2a31899b588956ea131
Josephine Heidepriem <jheidepriem@gmail.com>
2022-11-30 15:43:23 -0600

    add requires to Game class

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,9 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Round = require('../src/Round.js');
+const Card = require('../src/Card.js');
+const Deck = require('../src/Deck.js');
 
 class Game {
   constructor() {}
@@ -11,7 +14,7 @@ class Game {
   }
 
   printQuestion(round) {
-      util.main(round);
+    util.main(round);
   }
 }
 
