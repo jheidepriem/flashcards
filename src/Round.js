30482959c90a39a9edee80682bec81396dd10cc3
Josephine Heidepriem <jheidepriem@gmail.com>
2022-11-29 19:01:08 -0600

    add functions to return current card and take turns

--- src/Round.js
+++ src/Round.js
@@ -1,9 +1,33 @@
+const Turn = require("./Turn");
+
 class Round {
-  constructor(){
+  constructor(deck){
+    this.deck = deck;
+    this.turns = 0;
+    this.incorrectGuesses = [];
     
   }
+  
   returnCurrentCard(){
+    return this.deck.cards[this.turns]
+  }
+  
+  takeTurn(userGuess) {
+    let turn = new Turn(userGuess, this.returnCurrentCard())
+    this.turns++
+    turn.evaluateGuess()
+      if(turn.giveFeedback() === 'incorrect') {
+        this.incorrectGuesses.push(this.returnCurrentCard().id)
+    }
+    return turn.evaluateGuess();
+  }
+
+  calculatePercentCorrect() {
     
+  }
+
+  endRound() {
+
   }
 }
 
