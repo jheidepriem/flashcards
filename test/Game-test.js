c8a072bbf035c96d76cbbaf87f245ae85acd3940
Josephine Heidepriem <jheidepriem@gmail.com>
2022-11-30 15:44:15 -0600

    set up requires and beforeEach for game-class

---
+++ test/Game-test.js
@@ -0,0 +1,23 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Deck = require('../src/Deck.js');
+const Round = require('../src/Round.js');
+const Card = require('../src/Card.js');
+const Game = require('../src/Game.js')
+
+describe('Game', () => {
+
+  let card1;
+  let cards;
+  let deck;
+  let round;
+  let game;
+
+beforeEach(function() {
+  card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+  deck = new Deck(cards);
+  round = new Round(deck);
+  game = new Game(round)
+
+  })
+})
